8fa7e13d0888c227a7f8f039b1826e27e225b6de
Seongju Hwang <ajanab153@naver.com>
2022-07-05 18:21:46 +0900

    update 1:1 video call
    update 1:1 video call

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -7,11 +7,21 @@ const socket = io();
 const myFace = document.getElementById("myFace");
 const muteBtn = document.getElementById("mute");
 const cameraBtn = document.getElementById("camera");
-const cameraSelect = document.getElementById("cameras");
+const camerasSelect = document.getElementById("cameras");
+const welcome = document.getElementById("welcome");
+const welcomeForm = welcome.querySelector("form");
+
+//미디어와 관련된 요소 묶음
+const call = document.getElementById("call");
+
+call.hidden = true;
 
 let myStream;
 let muted = false;
 let cameraOff = false;
+let roomName;
+//연결객체
+let myPeerConnection;
 
 //모든 카메라 가져오기
 async function getCameras(){
@@ -27,14 +37,12 @@ async function getCameras(){
             if(currentCamera.label == camera.label){
                 option.selected = true;
             }
-            cameraSelect.appendChild(option);
-        })
+            camerasSelect.appendChild(option);
+        });
     }catch(e){
         console.log(e);
     }
 }
-
-
 
 //getUserMedia 메소드를 통한 스트림 반환은 비동기적 처리이므로
 //async-await로 처리 
@@ -43,28 +51,27 @@ async function getMedia(deviceId){
     //기본 디폴트 설정은 전면카메라
     const initialConstraints = {
         audio: true,
-        video: { facingMode : "user" }
+        video: { facingMode : "user" },
     };
     const cameraConstraints = {
         audio: true,
-        video: {deviceId: {exact: deviceId}}
-    };
+        video: {deviceId: {exact: deviceId}},
+    }
     
     try{
         myStream = await navigator.mediaDevices.getUserMedia(
-           deviceId ? cameraConstraints: initialConstraints
+            deviceId ? cameraConstraints: initialConstraints
         );
         //스트림으로 받아온 영상을 myFace에 넣는다
         myFace.srcObject = myStream;
         //select에서 선택할 때마다 반복해서 메뉴 추가되던 현상 해결
-        if(!deviceId)
+        if(!deviceId){
             await getCameras();
+        }
     } catch(e){
         console.log(e);
     }
 }
-
-getMedia();
 
 function handleMuteClick(){
     
@@ -81,7 +88,7 @@ function handleMuteClick(){
 
 function handleCameraClick(){
 
-    myStream.getVideoTracks().forEach((track) => (track.enabled = ! track.enabled));
+    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
 
     if(!cameraOff){
         cameraBtn.innerText = "Turn Camera On";
@@ -93,12 +100,101 @@ function handleCameraClick(){
 }
 
 //카메라 변경
-function handleCameraChange(){
-    await getMedia(cameraSelect.value);
+async function handleCameraChange(){
+    await getMedia(camerasSelect.value);
+    if(myPeerConnection){
+        const videoTrack = myStream.getVideoTracks()[0];
+        const videoSender = myPeerConnection
+        .getSenders()
+        .find((sender) => sender.track.kind==="video");
+        videoSender.replaceTrack(videoTrack);
+    }
 }
 
 
 muteBtn.addEventListener("click", handleMuteClick);
 cameraBtn.addEventListener("click", handleCameraClick);
 //input => 입력값이 변경될때 동작
-cameraSelect.addEventListner("input", handleCameraChange);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+//Welcome Form ( join a room )
+
+async function initCall(){
+    welcome.hidden = true;
+    call.hidden = false;
+    await getMedia();
+    makeConnection();
+}
+
+async function handleWelcomeSubmit(event){
+    event.preventDefault();
+    const input = welcomeForm.querySelector("input");
+    await initCall();
+    socket.emit("join_room", input.value);
+    roomName = input.value;
+    input.value = "";
+}
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit);
+
+//Socket Code
+
+//offer를 보내는 쪽에서 이벤트 핸들링
+socket.on("welcome", async ()=>{
+    const offer = await myPeerConnection.createOffer();
+    myPeerConnection.setLocalDescription(offer);    
+    socket.emit("offer", offer, roomName);
+})
+//offer를 받는 쪽에서 이벤트 핸들링
+socket.on("offer", async(offer) => {
+    myPeerConnection.setRemoteDescription(offer);
+    const answer = await myPeerConnection.createAnswer();
+    myPeerConnection.setLocalDescription(answer);
+    socket.emit("answer", answer, roomName);
+})
+
+socket.on("answer", answer => {
+    myPeerConnection.setRemoteDescription(answer);
+})
+
+socket.on("ice", ice => {
+    myPeerConnection.addIceCandidate(ice);
+});
+
+//RTC Code
+
+function makeConnection(){
+    //서로 접속한 와이파이가 다르던가 그러면 동작을 안해서 stun 서버가 필요
+    //stun 서버는 기기들이 공용 ip주소를 찾게해준다
+    //구글 무료 stun 서버 사용 => 실 상용화할때는 만들어야함
+    myPeerConnection = new RTCPeerConnection({
+        iceServers: [
+                {   
+                    urls:[
+                        "stun:stun.l.google.com:19302",
+                        "stun:stun1.l.google.com:19302",
+                        "stun:stun2.l.google.com:19302",
+                        "stun:stun3.l.google.com:19302",
+                        "stun:stun4.l.google.com:19302",
+                    ]
+                }
+        ]
+    });
+
+    myPeerConnection.addEventListener("icecandidate", handleIce);
+    myPeerConnection.addEventListener("addstream", handleAddStream);
+    myStream
+        .getTracks()
+        .forEach(track => myPeerConnection.addTrack(track, myStream))
+}
+
+function handleIce(data){
+    console.log("send icecandidate");
+    socket.emit("ice", data.candidate, roomName);
+    
+}
+
+function handleAddStream(data){
+    const peerFace1 = document.getElementById("peerFace");
+    peerFace1.srcObject = data.stream;
+}
